2222884e59f6760ef53e609a4c07f2dd6d685a37
hkkku <hhk7045@gmail.com>
2021-02-08 12:42:09 +0900

    02/08 헤더 및 메인 컨테이너 설계 초안

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Schedule</title>
+
+    <!-- reset css link -->
+    <link rel="stylesheet" href="/myschedule/css/reset.css" />
+    <!-- main css link -->
+    <link rel="stylesheet" href="/myschedule/css/style.css" />
+  </head>
+  <body>
+    <!-- all contents in wrap class -->
+    <div class="wrap">
+      <!-- header section -->
+      <header>
+        <div class="center headerWrap">
+          <span class="hollow"></span>
+          <h1>Schedule Dashboard</h1>
+          <div class="mIcon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </header>
+
+      <!-- container section -->
+      <div class="container"></div>
+    </div>
+  </body>
+</html>
