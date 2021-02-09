e52d6a59247542fe6723c47e69677df68dedb6d1
hkkku <hhk7045@gmail.com>
2021-02-09 12:50:14 +0900

    02/09 piechart 반응형 수정 & 진행도 바 수치 입력에 따라 애니메이트 되도록 개발

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Schedule</title>
 
+    <!--  Awesome Font link -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+    />
     <!-- reset css link -->
     <link rel="stylesheet" href="/myschedule/css/reset.css" />
     <!-- main css link -->
     <link rel="stylesheet" href="/myschedule/css/style.css" />
+    <link rel="stylesheet" href="/myschedule/css/media.css" />
   </head>
   <body>
     <!-- all contents in wrap class -->
@@ -41,10 +47,54 @@
               <p class="totalTit">Total Process</p>
             </section>
           </div>
-          <div class="item"></div>
-          <div class="item"></div>
-          <div class="item"></div>
-          <div class="item"></div>
+          <div class="item subPfBar">
+            <div class="pfImg">
+              <i class="fa fa-database"></i>
+            </div>
+            <div class="pfContents">
+              <h3>Database Project</h3>
+              <p>Progress Rate : <b class="rateNum">90</b>% || Last Update : <b>2021-02-09</b></p>
+              <div class="pfBarBox">
+                <span class="pfBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="item subPfBar">
+            <div class="pfImg">
+              <i class="fa fa-cloud-download-alt"></i>
+            </div>
+            <div class="pfContents">
+              <h3>API Project</h3>
+              <p>Progress Rate : <b class="rateNum">100</b>% || Last Update : <b>2021-02-09</b></p>
+              <div class="pfBarBox">
+                <span class="pfBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="item subPfBar">
+            <div class="pfImg">
+              <i class="fa fa-copy"></i>
+            </div>
+            <div class="pfContents">
+              <h3>Renewal Project</h3>
+              <p>Progress Rate : <b class="rateNum">30</b>% || Last Update : <b>2021-02-09</b></p>
+              <div class="pfBarBox">
+                <span class="pfBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="item subPfBar">
+            <div class="pfImg">
+              <i class="fa fa-edit"></i>
+            </div>
+            <div class="pfContents">
+              <h3>Web Planning Project</h3>
+              <p>Progress Rate : <b class="rateNum">10</b>% || Last Update : <b>2021-02-09</b></p>
+              <div class="pfBarBox">
+                <span class="pfBar"></span>
+              </div>
+            </div>
+          </div>
           <div class="item"></div>
         </div>
       </div>
@@ -53,5 +103,6 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>
    <script src="/myschedule/js/piechart.js"></script>
+   <script src="/myschedule/js/custom.js"></script>
   </body>
 </html>
--- js/piechart.js
+++ js/piechart.js
@@ -1,11 +1,52 @@
 $(function() {
+
+  let pieSize = 250;
+  let liWidth = $(window).width();
+  let clear;
+  
+  if(liWidth > 400){
+    pieSize = 250;
+  } else {
+    pieSize = 180;
+  }
+
+  console.log(pieSize);
+
   $('.circle-graph').easyPieChart({
     scaleColor: false,
     lineWidth: 30,
     lineCap: 'round',
     barColor: '#12595b',
     trackColor: '#edf7f0' ,
-    size: 250,
+    size: pieSize,
     animate: 1200
   });
+
+  $(window).resize(function(){
+    let liWidth = $(window).width();
+    //resize 할때마다 window의 넓이를 읽도록 함
+    if(liWidth > 400){
+      pieSize = 250;
+    } else {
+      pieSize = 180;
+    }
+
+    clearTimeout(clear);
+    // 한 타임씩 지워준다. 
+
+    clear = setTimeout(function(){
+      // 브라우저 과부하 방지 - resize가 너무 많을 때 사용
+      $('.circle-graph').removeData('easyPieChart').find('canvas').remove();
+      $('.circle-graph').easyPieChart({
+        scaleColor: false,
+        lineWidth: 30,
+        lineCap: 'round',
+        barColor: '#12595b',
+        trackColor: '#edf7f0' ,
+        size: pieSize,
+        animate: 1200
+      });
+    }, 150);
+  });
+
 });
