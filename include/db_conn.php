e7367a8f4df5f2645a0d08df068c395984dd4a77
hkkku <hhk7045@gmail.com>
2021-02-09 15:03:22 +0900

    02/09 DB와 input 태그 연결

---
+++ include/db_conn.php
@@ -0,0 +1,8 @@
+<?php
+  $id="root";
+  $pass='';
+  $db_name='test';
+  $host='localhost';
+
+  $db_conn = mysqli_connect($host, $id, $pass, $db_name);
+?>
--- index.php
+++ index.php
@@ -33,69 +33,90 @@
         </div>
       </header>
 
+      <?php
+        include $_SERVER['DOCUMENT_ROOT']."/myschedule/include/db_conn.php";
+        $sql = "select * from schedule_progress";
+
+        $sch_result = mysqli_query($db_conn, $sql);
+        $sch_row = mysqli_fetch_array($sch_result);
+
+        $sch_db = $sch_row['sch_db'];
+        $sch_api = $sch_row['sch_api'];
+        $sch_ren = $sch_row['sch_ren'];
+        $sch_plan = $sch_row['sch_plan'];
+      ?>
+
       <!-- container section -->
       <div class="center">
-        <div class="container">
-          <div class="item">
-            <section class="graph-circle">
-              <div class="circle-graph-container">
-                <div class="circle-graph easyPieChart" data-percent="65">
-                  <p>65%</p>
-                  <!-- <canvas width="269" height="269" style="width: 150px; height: 150px;"></canvas> -->
+        <form action="#" method="GET" name="updateRate">
+          <div class="container">
+            <div class="item">
+              <section class="graph-circle">
+                <div class="circle-graph-container">
+                  <div class="circle-graph easyPieChart" data-percent="65">
+                    <p>65%</p>
+                    <!-- <canvas width="269" height="269" style="width: 150px; height: 150px;"></canvas> -->
+                  </div>
+                </div>
+                <p class="totalTit">Total Process</p>
+              </section>
+            </div>
+          
+            <div class="item subPfBar">
+              <div class="pfImg">
+                <i class="fa fa-database"></i>
+              </div>
+              <div class="pfContents">
+                <h3>Database Project</h3>
+                <p>Progress Rate : <input type="text" value="<?=$sch_db?>" class="rateNum">%|| Last Update : <b>2021-02-09</b></p>
+                <div class="pfBarBox">
+                  <span class="pfBar"></span>
                 </div>
               </div>
-              <p class="totalTit">Total Process</p>
-            </section>
-          </div>
-          <div class="item subPfBar">
-            <div class="pfImg">
-              <i class="fa fa-database"></i>
             </div>
-            <div class="pfContents">
-              <h3>Database Project</h3>
-              <p>Progress Rate : <b class="rateNum">90</b>% || Last Update : <b>2021-02-09</b></p>
-              <div class="pfBarBox">
-                <span class="pfBar"></span>
+            <div class="item subPfBar">
+              <div class="pfImg">
+                <i class="fa fa-cloud-download-alt"></i>
+              </div>
+              <div class="pfContents">
+                <h3>API Project</h3>
+                <p>Progress Rate : <input type="text" value="<?=$sch_api?>" class="rateNum">% || Last Update : <b>2021-02-09</b></p>
+                <div class="pfBarBox">
+                  <span class="pfBar"></span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="item subPfBar">
-            <div class="pfImg">
-              <i class="fa fa-cloud-download-alt"></i>
-            </div>
-            <div class="pfContents">
-              <h3>API Project</h3>
-              <p>Progress Rate : <b class="rateNum">100</b>% || Last Update : <b>2021-02-09</b></p>
-              <div class="pfBarBox">
-                <span class="pfBar"></span>
+            <div class="item subPfBar">
+              <div class="pfImg">
+                <i class="fa fa-copy"></i>
+              </div>
+              <div class="pfContents">
+                <h3>Renewal Project</h3>
+                <p>Progress Rate : <input type="text" value="<?=$sch_ren?>" class="rateNum">% || Last Update : <b>2021-02-09</b></p>
+                <div class="pfBarBox">
+                  <span class="pfBar"></span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="item subPfBar">
-            <div class="pfImg">
-              <i class="fa fa-copy"></i>
-            </div>
-            <div class="pfContents">
-              <h3>Renewal Project</h3>
-              <p>Progress Rate : <b class="rateNum">30</b>% || Last Update : <b>2021-02-09</b></p>
-              <div class="pfBarBox">
-                <span class="pfBar"></span>
+            <div class="item subPfBar">
+              <div class="pfImg">
+                <i class="fa fa-edit"></i>
+              </div>
+              <div class="pfContents">
+                <h3>Web Planning Project</h3>
+                <p>Progress Rate : <input type="text" value="<?=$sch_plan?>" class="rateNum">% || Last Update : <b>2021-02-09</b></p>
+                <div class="pfBarBox">
+                  <span class="pfBar"></span>
+                </div>
               </div>
             </div>
+          </form>
+          <!-- updateRate form end -->
+          <div class="item btns">
+            <button type="button">진행률 수정</button>
+            <button type="button">진행 상황 작성</button>
+            <button type="button">진행 상황 확인</button>
           </div>
-          <div class="item subPfBar">
-            <div class="pfImg">
-              <i class="fa fa-edit"></i>
-            </div>
-            <div class="pfContents">
-              <h3>Web Planning Project</h3>
-              <p>Progress Rate : <b class="rateNum">10</b>% || Last Update : <b>2021-02-09</b></p>
-              <div class="pfBarBox">
-                <span class="pfBar"></span>
-              </div>
-            </div>
-          </div>
-          <div class="item"></div>
         </div>
       </div>
     </div>
